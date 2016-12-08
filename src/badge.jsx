cc12743448bc5f92820ffeae102f2b1d1769ee11
Dixit Patel <talkwithdixit@gmail.com>
2016-12-08 17:58:03 -0500

    Added Badge File and changed some code

--- src/badge.jsx
+++ src/badge.jsx
@@ -2,10 +2,12 @@ var React = require('react')
 
 var Badge = React.createClass({
   render: function() {
-    return <button className="btn btn-primary" type="button">
-      {this.props.title} <span className="badge">{this.props.number}</span>
+    return (
+      <button className="btn btn-primary" type="button">
+        {this.props.title} <span className="badge">{this.props.number}</span>
     </button>
-  }
+  )
+}
 })
 
 module.exports = Badge
